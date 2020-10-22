9235d8c76475378e35fca3da7157ea38e5092342
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-10-22 17:01:11 -0300

    initial setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import 'express-async-errors';
-
 import cors from 'cors';
+
+import routes from './routes/index.routes';
+
+import './database/connection';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server is running');
+});
