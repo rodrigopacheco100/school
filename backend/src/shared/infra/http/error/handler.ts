de8c3945292b0e57f5a6be86f6e1b2dd9cce016d
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-08 19:21:37 -0300

    error handler

--- backend/src/shared/infra/http/error/handler.ts
+++ backend/src/shared/infra/http/error/handler.ts
@@ -1,9 +1,28 @@
 import { ErrorRequestHandler } from 'express';
+import { format } from 'date-fns';
+import AppError from './AppError';
 
-const errorHandler: ErrorRequestHandler = (error, _, response) => {
-  console.error(error);
+const errorHandler: ErrorRequestHandler = (error, request, response, _) => {
+  const now = format(Date.now(), 'dd/MM/yyyy hh:mm:ss');
+  console.log({
+    timestamp: now,
+    url: `${request.method} ${request.headers.host}/${request.url}`,
+    headers: request.headers,
+    body: request.body,
+    params: request.params,
+    query: request.query,
+    error
+  });
 
-  return response.status(500).json({ message: 'Internal server error' });
+  if (error instanceof AppError)
+    return response
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  });
 };
 
 export default errorHandler;
