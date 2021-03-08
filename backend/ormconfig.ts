af19c11ddc80e6d4e922f08b163bc88bb4bcb234
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-08 19:17:28 -0300

    config changes

---
+++ backend/ormconfig.ts
@@ -0,0 +1,19 @@
+import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
+
+const config: MongoConnectionOptions = {
+  type: 'mongodb',
+  url: `mongodb${process.env.ENV === 'production' ? '+srv' : ''}://${
+    process.env.MONGODB_USER
+  }:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${
+    process.env.MONGODB_NAME
+  }?retryWrites=true&w=majority`,
+  database: 'school',
+  port: 27017,
+  synchronize: false,
+  logging: false,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  entities: ['src/modules/**/entity/*.ts']
+};
+
+export default config;
