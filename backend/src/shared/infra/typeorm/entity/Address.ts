14af1443b82c1cce7e6c61074681ef08448693f1
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-08 19:21:20 -0300

    address and contact classes

--- backend/src/shared/infra/typeorm/entity/Address.ts
+++ backend/src/shared/infra/typeorm/entity/Address.ts
@@ -1,4 +1,5 @@
-import { State } from '@shared/enums';
+import { State } from '@shared/types/enums';
+import Joi from 'joi';
 import { Column } from 'typeorm';
 
 export default class Address {
@@ -7,6 +8,9 @@ export default class Address {
 
   @Column()
   number: number;
+
+  @Column()
+  CEP: string;
 
   @Column()
   complement?: string;
@@ -20,3 +24,19 @@ export default class Address {
   @Column()
   state: State;
 }
+
+type Schema = {
+  [k in keyof Address]: Joi.Schema;
+};
+
+export const addressJoiSchema: Schema = {
+  street: Joi.string().required(),
+  number: Joi.number().required(),
+  complement: Joi.string().optional(),
+  city: Joi.string().required(),
+  neighborhood: Joi.string().required(),
+  CEP: Joi.string().length(9),
+  state: Joi.string()
+    .valid(...Object.values(State))
+    .required()
+};
