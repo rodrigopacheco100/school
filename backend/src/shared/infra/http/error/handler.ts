4020cdb1fa101b0c6c4d719f16c8f390692e9ed6
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-14 13:24:39 -0300

    printWidth changed to 120

--- backend/src/shared/infra/http/error/handler.ts
+++ backend/src/shared/infra/http/error/handler.ts
@@ -15,9 +15,7 @@ const errorHandler: ErrorRequestHandler = (error, request, response, _) => {
   });
 
   if (error instanceof AppError)
-    return response
-      .status(error.statusCode)
-      .json({ status: 'error', message: error.message });
+    return response.status(error.statusCode).json({ status: 'error', message: error.message });
 
   return response.status(500).json({
     status: 'error',
