b0445699a6ed8b9b4358c0a69dbb22be2aa5b380
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-19 14:05:59 -0300

    printWidth changed to 120

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -2,11 +2,9 @@ import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOp
 
 const config: MongoConnectionOptions = {
   type: 'mongodb',
-  url: `mongodb${process.env.ENV === 'production' ? '+srv' : ''}://${
-    process.env.MONGODB_USER
-  }:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${
-    process.env.MONGODB_NAME
-  }?retryWrites=true&w=majority`,
+  url: `mongodb${process.env.ENV === 'production' ? '+srv' : ''}://${process.env.MONGODB_USER}:${
+    process.env.MONGODB_PASSWORD
+  }@${process.env.MONGODB_HOST}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`,
   database: 'school',
   port: 27017,
   synchronize: false,
