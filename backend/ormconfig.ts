7d3899890599b076ca2a22769b4fc2bfcfa8917d
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-23 11:25:21 -0300

    updating ormconfig

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,17 +1,25 @@
 import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
 
-const config: MongoConnectionOptions = {
-  type: 'mongodb',
-  url: `mongodb${process.env.ENV === 'production' ? '+srv' : ''}://${process.env.MONGODB_USER}:${
-    process.env.MONGODB_PASSWORD
-  }@${process.env.MONGODB_HOST}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`,
-  database: 'school',
-  port: 27017,
-  synchronize: false,
-  logging: false,
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  entities: ['src/modules/**/entity/*.ts']
-};
-
+const config: MongoConnectionOptions =
+  process.env.ENV === 'local'
+    ? {
+        type: 'mongodb',
+        username: process.env.MONGODB_USER,
+        password: process.env.MONGODB_PASSWORD,
+        host: process.env.MONGODB_HOST,
+        database: process.env.MONGODB_NAME,
+        synchronize: false,
+        logging: false,
+        useUnifiedTopology: true,
+        entities: ['src/modules/**/entity/*.ts']
+      }
+    : {
+        type: 'mongodb',
+        url: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`,
+        synchronize: false,
+        logging: false,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        entities: ['src/modules/**/entity/*.ts']
+      };
 export default config;
