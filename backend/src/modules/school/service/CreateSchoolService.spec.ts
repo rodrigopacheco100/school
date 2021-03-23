da13273e3cabb37bdad71e47e75d05ea7d712d52
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-03-23 14:24:12 -0300

    create school tests

---
+++ backend/src/modules/school/service/CreateSchoolService.spec.ts
@@ -0,0 +1,188 @@
+import 'reflect-metadata';
+import { State } from '@shared/types/enums';
+import AppError from '@shared/infra/http/error/AppError';
+import FakeSchoolRepository from '../repository/fakes/FakeSchoolRepository';
+import ISchoolRepository from '../repository/ISchoolRepository';
+import CreateSchoolService from './CreateSchoolService';
+
+describe('CreateSchool', () => {
+  let schoolRepository: ISchoolRepository;
+  let createSchoolService: CreateSchoolService;
+
+  beforeEach(() => {
+    schoolRepository = new FakeSchoolRepository();
+    createSchoolService = new CreateSchoolService(schoolRepository);
+  });
+
+  it('should create a school', async () => {
+    const school = await createSchoolService.execute({
+      username: 'johndoe',
+      password: 'password',
+      name: 'John Doe',
+      cnpj: '95983747000113',
+      confirmedAt: null,
+      contact: {
+        email: 'johndoe@email.com'
+      },
+      address: {
+        cep: '99999-000',
+        city: 'Cidade',
+        neighborhood: 'Bairro',
+        number: 123,
+        state: State.Distrito_Federal,
+        street: 'Rua'
+      }
+    });
+
+    expect(school).toHaveProperty('_id');
+  });
+
+  it('should not create a school with same username', async () => {
+    await createSchoolService.execute({
+      username: 'johndoe',
+      password: 'password',
+      name: 'John Doe',
+      cnpj: '95983747000113',
+      confirmedAt: null,
+      contact: {
+        email: 'johndoe@email.com'
+      },
+      address: {
+        cep: '99999-000',
+        city: 'Cidade',
+        neighborhood: 'Bairro',
+        number: 123,
+        state: State.Distrito_Federal,
+        street: 'Rua'
+      }
+    });
+
+    await expect(
+      createSchoolService.execute({
+        username: 'johndoe',
+        password: 'password',
+        name: 'John Doe',
+        cnpj: '40042391000121',
+        confirmedAt: null,
+        contact: {
+          email: 'johndoe@email.com.br'
+        },
+        address: {
+          cep: '99999-000',
+          city: 'Cidade',
+          neighborhood: 'Bairro',
+          number: 123,
+          state: State.Distrito_Federal,
+          street: 'Rua'
+        }
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not create a school with invalid cnpj', async () => {
+    await expect(
+      createSchoolService.execute({
+        username: 'doejohn',
+        password: 'password',
+        name: 'John Doe',
+        cnpj: '95983747009999',
+        confirmedAt: null,
+        contact: {
+          email: 'johndoe@email.com.br'
+        },
+        address: {
+          cep: '99999-000',
+          city: 'Cidade',
+          neighborhood: 'Bairro',
+          number: 123,
+          state: State.Distrito_Federal,
+          street: 'Rua'
+        }
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not create a school with same cnpj', async () => {
+    await createSchoolService.execute({
+      username: 'johndoe',
+      password: 'password',
+      name: 'John Doe',
+      cnpj: '95983747000113',
+      confirmedAt: null,
+      contact: {
+        email: 'johndoe@email.com'
+      },
+      address: {
+        cep: '99999-000',
+        city: 'Cidade',
+        neighborhood: 'Bairro',
+        number: 123,
+        state: State.Distrito_Federal,
+        street: 'Rua'
+      }
+    });
+
+    await expect(
+      createSchoolService.execute({
+        username: 'doejohn',
+        password: 'password',
+        name: 'John Doe',
+        cnpj: '95983747000113',
+        confirmedAt: null,
+        contact: {
+          email: 'johndoe@email.com.br'
+        },
+        address: {
+          cep: '99999-000',
+          city: 'Cidade',
+          neighborhood: 'Bairro',
+          number: 123,
+          state: State.Distrito_Federal,
+          street: 'Rua'
+        }
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not create a school with same email', async () => {
+    await createSchoolService.execute({
+      username: 'johndoe',
+      password: 'password',
+      name: 'John Doe',
+      cnpj: '95983747000113',
+      confirmedAt: null,
+      contact: {
+        email: 'johndoe@email.com'
+      },
+      address: {
+        cep: '99999-000',
+        city: 'Cidade',
+        neighborhood: 'Bairro',
+        number: 123,
+        state: State.Distrito_Federal,
+        street: 'Rua'
+      }
+    });
+
+    await expect(
+      createSchoolService.execute({
+        username: 'doejohn',
+        password: 'password',
+        name: 'John Doe',
+        cnpj: '40042391000121',
+        confirmedAt: null,
+        contact: {
+          email: 'johndoe@email.com'
+        },
+        address: {
+          cep: '99999-000',
+          city: 'Cidade',
+          neighborhood: 'Bairro',
+          number: 123,
+          state: State.Distrito_Federal,
+          street: 'Rua'
+        }
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
