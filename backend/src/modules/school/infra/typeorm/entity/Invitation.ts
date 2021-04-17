c36a4e2c3f06c3923f125f6f9361703df5c30321
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-04-17 14:21:52 -0300

    invitation entity

--- backend/src/modules/school/infra/typeorm/entity/Invitation.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectID } from 'mongodb';
-import { Entity, ObjectIdColumn } from 'typeorm';
-
-@Entity('Invitation')
-export default class Invitation {
-  @ObjectIdColumn()
-  _id: ObjectID;
-}
