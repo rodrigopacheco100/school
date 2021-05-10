758431555ab43d8cc8af33f08b171c49ec2147e0
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-05-09 23:45:10 -0300

    modifying app to be testable

--- workspaces/backend/src/shared/container/index.ts
+++ workspaces/backend/src/shared/container/index.ts
@@ -2,13 +2,29 @@ import { container } from 'tsyringe';
 
 import ISchoolRepository from '@modules/school/repository/ISchoolRepository';
 import SchoolRepository from '@modules/school/infra/typeorm/repository/SchoolRepository';
+import FakeSchoolRepository from '@modules/school/repository/fakes/FakeSchoolRepository';
+
 import ITeacherRepository from '@modules/teacher/repository/ITeacherRepository';
 import TeacherRepository from '@modules/teacher/infra/typeorm/repository/TeacherRepository';
+import FakeTeacherRepository from '@modules/teacher/repository/fakes/FakeTeacherRepository';
+
 import IStudentRepository from '@modules/student/repository/IStudentRepository';
 import StudentRepository from '@modules/student/infra/typeorm/repository/StudentRepository';
+import FakeStudentRepository from '@modules/student/repository/fakes/FakeStudentRepository';
 
 export default (): void => {
-  container.registerSingleton<ISchoolRepository>('SchoolRepository', SchoolRepository);
-  container.registerSingleton<ITeacherRepository>('TeacherRepository', TeacherRepository);
-  container.registerSingleton<IStudentRepository>('StudentRepository', StudentRepository);
+  container.registerSingleton<ISchoolRepository>(
+    'SchoolRepository',
+    process.env.NODE_ENV !== 'TEST' ? FakeSchoolRepository : SchoolRepository
+  );
+
+  container.registerSingleton<ITeacherRepository>(
+    'TeacherRepository',
+    process.env.NODE_ENV !== 'TEST' ? FakeTeacherRepository : TeacherRepository
+  );
+
+  container.registerSingleton<IStudentRepository>(
+    'StudentRepository',
+    process.env.NODE_ENV !== 'TEST' ? FakeStudentRepository : StudentRepository
+  );
 };
--- workspaces/backend/src/shared/infra/http/app.ts
+++ workspaces/backend/src/shared/infra/http/app.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Application } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { Connection } from 'typeorm';
 
 import connectDB from '@shared/infra/typeorm/connection';
 import containers from '@shared/container';
@@ -9,10 +10,12 @@ import containers from '@shared/container';
 import errorHandler from './error/handler';
 import routes from './routes';
 
-export default async (): Promise<void> => {
+export default async (): Promise<Application> => {
   const app = express();
 
-  const connection = await connectDB();
+  let connection: Connection = null;
+  if (process.env.NODE_ENV !== 'TEST') connection = await connectDB();
+
   containers();
 
   app.use(cors());
@@ -21,5 +24,7 @@ export default async (): Promise<void> => {
   app.use(routes);
   app.use(errorHandler);
 
-  if (connection) app.listen(3333, () => console.log('Server is running'));
+  if (process.env.NODE_ENV === 'TEST' || connection) app.listen(3333, () => console.log('Server is running'));
+
+  return app;
 };
