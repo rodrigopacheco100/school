bab0e3d0c48e081e7022c00e27e695129b69c7d3
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2021-06-01 00:34:46 -0300

    fixing createSchool tests

--- workspaces/backend/src/modules/school/infra/http/core/createSchool.spec.ts
+++ workspaces/backend/src/modules/school/infra/http/core/createSchool.spec.ts
@@ -1,8 +1,9 @@
 import request from 'supertest';
 import app from '@shared/infra/http/app';
+import CreateSchoolDTO from '@modules/school/dtos/CreateSchoolDTO';
 import { State } from '@shared/types/enums';
 import { Application } from 'express';
-import CreateSchoolDTO from '@modules/school/dtos/CreateSchoolDTO';
+import { Omit } from '@shared/types/utilTypes';
 
 describe('/school/createSchool', () => {
   let application: Application;
@@ -34,25 +35,21 @@ describe('/school/createSchool', () => {
   });
 
   it('should not be able to create a school with wrong schema', async () => {
-    const response = await request(application)
-      .post('/school/createSchool')
-      .send({
-        username: 'johndoe',
-        password: 'password',
-        name: 'John Doe',
-        confirmedAt: null,
-        contact: {
-          email: 'johndoe@email.com'
-        },
-        address: {
-          cep: '99999-000',
-          city: 'Cidade',
-          neighborhood: 'Bairro',
-          number: 123,
-          state: State.Distrito_Federal,
-          street: 'Rua'
-        }
-      });
+    const body: Omit<CreateSchoolDTO, 'cnpj'> = {
+      password: 'password',
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      address: {
+        cep: '99999-000',
+        city: 'Cidade',
+        neighborhood: 'Bairro',
+        number: 123,
+        state: State.Distrito_Federal,
+        street: 'Rua'
+      }
+    };
+
+    const response = await request(application).post('/school/createSchool').send(body);
 
     expect(response.status).toBe(400);
   });
